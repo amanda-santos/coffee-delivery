bdd66673c28badb15f91428e6ee93341aaec8902
amanda-santos <amandasf0200@gmail.com>
2022-08-09 17:44:00 -0300

    feat: 💄 add background image on landing page

--- src/components/LandingPageInformation/styles/LandingPageInformationContainer.ts
+++ src/components/LandingPageInformation/styles/LandingPageInformationContainer.ts
@@ -7,6 +7,8 @@ export const LandingPageInformationContainer = styled.div`
   align-items: center;
   justify-content: space-between;
 
+  background-image: url("src/assets/landing-page-background.svg");
+
   padding: 9.6rem 16.8rem;
 
   @media screen and (max-width: ${({ theme }) => theme.breakpoints.xl}) {
