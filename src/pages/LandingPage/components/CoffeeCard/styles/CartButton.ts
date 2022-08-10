243bcbbc9027f01d3ec2cb1d46141b78bb4f9ade
amanda-santos <amandasf0200@gmail.com>
2022-08-10 20:00:47 -0300

    feat: 💄 add hover state on cart button

--- src/pages/LandingPage/components/CoffeeCard/styles/CartButton.ts
+++ src/pages/LandingPage/components/CoffeeCard/styles/CartButton.ts
@@ -4,11 +4,19 @@ export const CartButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+
   background-color: ${({ theme }) => theme.colors["secondary-dark"]};
   color: ${({ theme }) => theme.colors["base-card"]};
 
   border-radius: 0.6rem;
   border-color: transparent;
-
   padding: 0.4rem;
+
+  cursor: pointer;
+
+  transition: all 0.125s ease-in-out;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors["secondary-base"]};
+  }
 `;
